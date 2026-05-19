12338309dde792816ea0eb6941ebf3ee2f881e9f
Ikteaja Hasan <ikteaja.hasan@rldatix.com>
2026-05-19 15:31:33 +0200

    fix:empty are src init file

--- src/__init__.py
+++
@@ -1,2 +0,0 @@
-#This is a special Python file that tells Python "this folder is a Python package — you can import code from it."
-#Without this file, Python cannot find your code when you do from src.rag.retriever import something.
